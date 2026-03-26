35f23ebc0509330255e19ac49e1fa0e3b2ce425b
alexbababu <alexander.janisch@gmail.com>
2026-03-26 09:05:07 +0100

    map and correction from support

--- Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
+++ Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
@@ -264,7 +264,7 @@ int main(int argc, char **argv) {
         if ((detection.x * detection.x + detection.y * detection.y +
              detection.z * detection.z) > 8.0) {
           pclCloud.points.push_back(
-              PointT(detection.x, detection.y, detection.z));
+              PointT(-detection.y, detection.x, -detection.z));
         }
       }
       if (pclCloud.points.size() > 5000) { // CANDO: Can modify this value to
